e24e947e5eb00e5d261e3738e244545a34f12842
Marlon Lazzarotti <mlazzarotti@gmail.com>
2023-05-25 23:14:35 -0300

    Add files via upload

--- roteiro.py
+++ roteiro.py
@@ -1,13 +1,54 @@
 '''
 LINGUAGEM DE PROGRAMACAO
+é um tipo de linguagem que a gente utiliza para programar computadores e máquinas, dando
+tarefas específicas que elas possam realizar.
 
 PYTHON
+É um tipo de linguagem atual, de alto nível, simplificada e estruturada com uma sintaxe
+mais simples do geralmente a gente encontra em outras linguagens.
 
 IDE - AMBIENTE DE DESENVOLVIMENTO
 
+É um aplicativo/software que auxilia o desenvolvimento de algoritmos utilizados para programar
+numa linguagem de programação específica. Dentro desse aplicativo, temos bibliotecas, atalhos,
+plugins que servem para dinamizar e facilitar a vida do programador. Na IDE também conseguimos
+fazer a compilação e interpretação do código automaticamente ou de forma mais fácil.
+
 ESTRUTURA BASICA DE UM PROGRAMA PYTHON
 
+basic.py
+
 VARIAVEIS E TIPOS DE DADOS
+
+Variavel é uma forma de armazenar um valor especifico. 
+Temos vários tipos, como INT, FLOAT, STRING, BOOLEAN E LISTAS...
+
+nome = 'Mateus Silva'  #string
+idade = 28              #int
+nota = 7.5              #float
+passou_de_ano = True    #boolean
+
+e as listas????
+Lista é um conjunto de elementos:
+
+CRIANDO UMA LISTA E MOSTRANDO UM ELEMENTO DA LISTA A PARTIR DO SEU INDICE
+lista = [10, 20, 30, 40, 50]
+print(lista[2])
+>30
+
+TROCANDO UM VALOR DE UM ELEMENTO DA LISTA POR UM NOVO VALOR
+lista[0] = 3
+lista = [3, 20, 30, 40, 50]
+
+ADICIONANDO E REMOVENDO ELEMENTOS E CONTAGEM DE UM ELEMENTO ESPECIFICO
+lista = [1, 2, 3]
+lista.append(4)  # [1, 2, 3, 4]
+lista.remove(2)  # [1, 3, 4]
+count = lista.count(1)  # 1
+
+FATIANDO A LISTA
+lista = [1, 2, 3, 4, 5]
+sublista = lista[1:4]  # [2, 3, 4]
 
 ESTRUTURAS DE CONTROLE
 
